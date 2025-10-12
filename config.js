991754c67cd959ced419ab5af59fbb0fcf039405
Minabasem23 <minabasem70@gmail.com>
2025-10-12 13:49:57 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const ADSGRAM_CONFIG = {
+    token: "3b09764c632c4c0687ef0d63894e8a3f",     // API Token
+    publisher_id: "38461",                         // Publisher ID الجديد
+    widget_id: "int-16035",                        // Widget/Block ID
+    base_url: "https://api.adsgram.ai"
+};
