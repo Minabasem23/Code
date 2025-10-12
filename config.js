e2aa2d36ce5982b829849f24fed4a2d694a11e1d
Minabasem23 <minabasem70@gmail.com>
2025-10-12 14:50:52 +0300

    Update and rename config.js to Proxy.php

--- config.js
+++
@@ -1,6 +0,0 @@
-const ADSGRAM_CONFIG = {
-    token: "3b09764c632c4c0687ef0d63894e8a3f",
-    publisher_id: "38461",                    
-    widget_id: "int-16032",  // ✅ هذا هو Block ID من لوحة التحكم
-    base_url: "https://api.adsgram.ai"
-};
