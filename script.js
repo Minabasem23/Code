cee0d78f612a126f1e831c08c205a181394a9c06
Minabasem23 <minabasem70@gmail.com>
2025-12-03 00:11:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+copyBtn.addEventListener("click", ()=>{
+  const text = div.querySelector(".msg-text").textContent;
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  try{
+    document.execCommand('copy'); // يعمل على معظم المتصفحات
+    alert("تم النسخ!");
+  } catch(err){
+    alert("فشل النسخ");
+  }
+  document.body.removeChild(textarea);
+  toolbar.remove();
+});
