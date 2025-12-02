835622bcbb73e06586bbd5cdfc67861f3b53fd1e
Minabasem23 <minabasem70@gmail.com>
2025-12-02 23:46:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-audio.addEventListener("ended", ()=>{
-  playBtn.innerHTML = "▶️"; // إعادة الزر لوضع Play
-  progressFilled.style.width = "0%"; // إعادة شريط التقدم للصفر
-  audio.currentTime = 0; // إعادة المؤشر للصفر
-});
+
