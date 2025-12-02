25f4c6dbd014207ba5dd79e67e86c110c410ccdf
Minabasem23 <minabasem70@gmail.com>
2025-12-02 23:30:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function addAudioMessage(url){
-  const div = document.createElement("div");
-  div.className = "audio";
 
-  const audioEl = document.createElement("audio");
-  audioEl.controls = true;
-  audioEl.src = url;
-
-  // جعل مشغل الصوت صغير
-  audioEl.style.width = "120px";  // عرض صغير مناسب للهاتف
-  audioEl.style.height = "25px";  // ارتفاع صغير
-  audioEl.style.borderRadius = "8px"; // اختياري لمظهر أفضل
-
-  div.appendChild(audioEl);
-  chat.appendChild(div);
-  chat.scrollTop = chat.scrollHeight;
-}
